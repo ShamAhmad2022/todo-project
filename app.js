08fa1d1d0fde4020c9007134d9bf62c5b77578d5
ShamAhmad2022 <shamahmadaljalam@gmail.com>
2023-04-12 18:23:52 +0000

    add changes to app.js file for lab06

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
+'use strict';
+
+//lab 05
+alert("lab 05 started");
+
 let userName = prompt("please enter your name");
 
 let userGender = prompt("please enter your gender", "male or female");
@@ -21,3 +26,42 @@ if (answer == false) {
     console.log(3);
   }
 }
+
+//lab 06
+alert("lab 06 started");
+alert("note: You can track the process in the console");
+
+const checkValue =(Ques) =>{
+  
+  if(Ques.toLocaleLowerCase()===''){
+    Ques='invalid';  
+    return Ques;
+  }
+
+  return Ques;
+
+}
+
+const addToArray =(Arr, Ques) =>{
+  
+  Arr.push(Ques);
+  return Arr;
+
+}
+
+const arrOfAnswers = new Array();
+
+let question1 = prompt("Is HTML a shortcut for HyperText Markup Language?", 'Yes or No');
+console.log('question 1 answer: ', checkValue(question1));
+console.log('Arr: ', addToArray(arrOfAnswers, checkValue(question1)));
+
+let question2 = prompt("Is CSS a shortcut for Cascading Style Sheets?", 'Yes or No');
+console.log('question 2 answer: ', checkValue(question2));
+console.log('Arr: ', addToArray(arrOfAnswers, checkValue(question2)));
+
+let question3= prompt("Is JS a shortcut for Javascript?", 'Yes or No');
+console.log('question 3 answer: ', checkValue(question3));
+console.log('Arr: ', addToArray(arrOfAnswers, checkValue(question3)));
+
+console.log('The array of answers: ',arrOfAnswers);
+alert(`Your answers are: (${arrOfAnswers})`);
